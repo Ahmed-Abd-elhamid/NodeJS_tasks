15aa35c5631708d80202ab07e9e84d2ec6e7a5ba
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-08-12 23:44:40 +0200

    node.js events

---
+++ events.js
@@ -0,0 +1,33 @@
+console.log(`Path to file: { Dir: ${__dirname}, file:${__filename}}`);
+
+let events = require('events');
+let util = require('util');
+
+// constructor function
+let Person = function(name, age, gender){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+}
+
+// extend events.EventEmitter to Person function (Inheritance)..
+util.inherits(Person, events.EventEmitter);
+
+// new objects (people) from constructor function
+let ahmed = new Person("ahmed", 25, "male");
+let ali = new Person("ali", 20, "male");
+let nour = new Person("nour", 22, "female");
+
+let people = new Array(ahmed, ali, nour);
+
+// Add Event Listener to the people ..
+people.forEach((person)=>{
+    person.on('speak', function(msg){
+        console.log(`${person.name} said: ${msg}`);
+    });
+});
+
+// Emit (Trigger) The Event ..
+ahmed.emit('speak', 'he said the truth');
+nour.emit('speak', 'no, he is not!');
+
