626bb1b331c741fc9d128052dc15381bdd892507
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-08-13 00:41:40 +0200

    response with html file in serverjs

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 console.log(`Path to file: { Dir: ${__dirname}, file:${__filename}}`);
 
 let http = require('http');
-// let html = require('./index.html');
+let fs = require('fs');
 
-let server = http.createServer( (request, response) => {
-    console.log(`Request URL: ${request.url}`);
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end('Index')
+let server = http.createServer((request, response) => {
+    if (request.url == '/') {
+        console.log(`Request URL: ${request.url}`);
+        response.writeHead(200, { "Content-Type": "text/html" });
+        let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+        myReadStream.pipe(response);
+    } else {
+        console.log(`Request URL: ${request.url}`);
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        let myReadStream = fs.createReadStream(__dirname + '/FileSystem/files/stream/readStream.txt', 'utf8');
+        myReadStream.pipe(response);
+    }
+    // response.end('Index')
 });
 
 console.log("Listening to localhost:2000");
