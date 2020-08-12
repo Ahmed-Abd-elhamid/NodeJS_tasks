4861fe98d5f9006ba111337687d970a1cc629f22
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-08-13 01:07:04 +0200

    read and write stream

---
+++ FileSystem/stream.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+
+// ReadStream and WriteStream
+let myReadStream = fs.createReadStream(`${__dirname}/files/stream/readStream.txt`,"utf8");
+let myWriteStream = fs.createWriteStream(`${__dirname}/files/stream/writeStream.txt`,"utf8");
+
+myReadStream.on('data', function(chunk){
+    console.log("\n\nChuck Received:\n");
+    console.log(chunk);
+
+    myWriteStream.write(chunk);
+    console.log("\nWrite Stream Done");
+})
+
+// ##OR##
+
+myReadStream.pipe(myReadStream);
