eb429081f31f887b22da982e6f03afd5ff265ae4
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-08-13 14:23:26 +0200

    working on <ejs package> <body-parser package> with <express package

--- npm/app.js
+++ npm/app.js
@@ -1,17 +1,46 @@
 // require('./server');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
-    res.send('This is the main Index');
+    // res.send('this is index');
+    res.sendFile(`${__dirname}/templates/index.html`);
 });
 
 app.get('/profile/:id', (req, res) => {
-    res.send(`Your Request the ID: ${req.params.id}`)
+    // res.send(`Your Request the ID: ${req.params.id}`);
+    let data = {
+        id: req.params.id,
+        name: "ahmed",
+        age: 25,
+        hobbies: ['playing', 'fishing', 'chess'],
+    }
+    res.render('profile', { data: data });
 })
 
+app.get('/contact', (req, res) => {
+    // res.send(`Your Request the ID: ${req.params.id}`);
+    let query = req.query;
+    console.log(query);
+
+    res.render('contact', { query: query });
+})
+
+app.post('/contact-success', urlencodedParser, (req, res) => {
+    // res.send(`Your Request the ID: ${req.params.id}`);
+    let data = req.body;
+    console.log(data);
+
+    res.render('contact-success', { data: data });
+})
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Sever up on port: ${PORT}`)
 });
